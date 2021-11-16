426ae4b5127262ae017c08941c083197c1378447
nikob <nikolaj.bauer@uzh.ch>
2021-11-16 16:35:48 +0100

    Creating the database with 1000 common nouns

--- Database/create_database.py
+++ Database/create_database.py
@@ -1,5 +1,40 @@
 from urllib.parse import urlparse
 import psycopg2
+from PyDictionary import PyDictionary
+import wordfreq
+from pandas import read_csv
+
+dictionary = PyDictionary()
+
+df = read_csv('most-common-nouns-english.csv') # id word plural
+# for word in df['Word']:
+#     print(word, dictionary.meaning(word)['Noun'][0], wordfreq.zipf_frequency(word, 'en'))
+
+
+# same definitions as in PyDictionary, but could be useful for hints (Exampl usage)
+# from nltk.corpus import wordnet
+# syns = wordnet.synsets("walk")
+# print("Defination of the said word:")
+# print(syns[0].definition())
+# print("\nExamples of the word in use::")
+# print(syns[0].examples())
+
+
+
+# Words to use: ca 1000, nouns and verbs
+
+# for i, word in enumerate(wordfreq.iter_wordlist('en', wordlist='best')):
+#     print(word, wordfreq.zipf_frequency(word, 'en'))
+#     print(dictionary.meaning(word))
+#     if i > 10:
+#         break
+
+
+
+# Todo:
+    # get the words from the Pydictionary and feed them into the db
+    # https://github.com/geekpradd/PyDictionary
+
 
 # getting the database from the uri:
 result = urlparse("postgres://postgres:22b216c54a2bd9c527c5d340b5b9901f@172.23.115.113:55079/team_a_db")
@@ -27,3 +62,22 @@ connection = psycopg2.connect(
     host = hostname,
     port = port
 )
+cur = connection.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS words (id int PRIMARY KEY, word text, definition text, Zipf_freq float)''')
+sql = '''INSERT INTO words (id, word, definition, zipf_freq)
+            Values (%s, %s, %s, %s); '''
+
+for i, word in enumerate(df['Word']):
+    try:
+        data = i, word, dictionary.meaning(word)['Noun'][0], wordfreq.zipf_frequency(word, 'en')
+
+        cur.execute(sql, data)
+    except:
+        continue
+
+connection.commit()
+cur.close()
+connection.close()
+# for i, word in enumerate(df['Word']):
+#     cur.execute('''INSERT INTO words''')
+# # df.to_sql('characters', connection, if_exists='replace', index=False)
