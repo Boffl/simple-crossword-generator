c4c20ed24f0e4a46323874c3a7e93b42c1717171
Luisa Wunderlin <luisa.wunderlin@uzh.ch>
2021-11-19 16:03:19 +0100

    Update settings.py, allowed host

--- crossword_generator/crossword_generator/settings.py
+++ crossword_generator/crossword_generator/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure--=)wzw9#6zfy+gfhlzpb1%))54xser(m*1j7ab+$ve=@dr8f0^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.23.115.113']
 
 
 # Application definition
