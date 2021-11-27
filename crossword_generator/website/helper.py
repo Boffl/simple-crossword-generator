e771df988ab0e7affd6629a083a70e5e7122bf3a
luisa <luisa.wunderlin@uzh.ch>
2021-11-27 17:14:46 +0100

    introduced helper functions, cleaned up

---
+++ crossword_generator/website/helper.py
@@ -0,0 +1,63 @@
+
+
+
+class div_crossword():
+
+    def __init__(self, obj_list, obj_size):
+        self.obj_list = obj_list
+        self.obj_size = obj_size
+
+        # dimensions of crossword: hxw
+        h, w = self.obj_size
+        cw_list = self.obj_list
+
+        self.empty_html = self.empty_div(h, w, cw_list)
+        self.filled_html = self.filled_div(h, w, cw_list)
+
+
+    def element_empty(self):
+        """ blank white div container"""
+        return "<div style='width:30px; height:30px; border:thin solid; background:white; padding: 0; margin: 0;'> </div>"
+
+    def element_black(self):
+        return "<div style='width:30px; height:30px; border:thin solid; background:black; padding: 0; margin: 0;'> </div>"
+
+
+    def empty_div(self, h, w, cw_list):
+        """ Creates the HTML syntax for the empty crossword """
+        empty_div = ""
+        for row in range(h):
+            empty_div = empty_div + "<div class ='row' style='padding:0; margin:0'>"
+            for col in range(w):
+                if cw_list[row][col] == '#':
+                    empty_div = empty_div + self.element_black()
+                else:
+                    empty_div = empty_div + self.element_empty()
+            empty_div = empty_div + '</div>'
+        return empty_div
+
+
+    def filled_div(self, h, w, cw_list):
+        """ Creates the HTML syntax for the filled crossword """
+        filled_div = ""
+        for row in range(h):
+            filled_div = filled_div + "<div class ='row' style='padding:0; margin:0'>"
+            for col in range(w):
+                if cw_list[row][col] == '#':
+                    filled_div = filled_div + self.element_black()
+                else:
+                    filled_div = filled_div + "<div class ='col-3' style='width:30px; height:30px; border:thin solid; background:white; padding: 0; margin: 0;'>" \
+                                 + cw_list[row][col] + "</div>"
+            filled_div = filled_div + "</div>"
+        return filled_div
+
+
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    # explicit function
+    def method():
+        print("GFG")
--- crossword_generator/website/views.py
+++ crossword_generator/website/views.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append("..")
 from Database.crossword_generation_15_11_21 import crossword_generator
 from .models import Words3
+from .helper import div_crossword
 
 
 # Create your views here.
@@ -13,7 +14,7 @@ def index(request):
     definition_list = []
 
     """ Import Data """
-    for i in range(1, 11):
+    for i in range(1, 15):
         # get a random word
         temp_obj = Words3.objects.order_by('?').first()
         temp_name = temp_obj.word
@@ -22,7 +23,6 @@ def index(request):
         # save data to lists
         word_list.append(temp_name)
         definition_list.append([i, temp_def])
-
 
     """ Create Crosword """
     # obj: crossword object
@@ -35,45 +35,22 @@ def index(request):
         fetched_words = fetched_words + str(definition_list[i][0]) + "  " + definition_list[i][1] + "<br>"
 
 
-    """ Create Empty Crossword """
+    """ Create Crossword """
     # dimensions of crossword: hxw
     h, w = obj.size()
     cw_list = obj.crossword
-
-    empty_div = ""
-    for row in range(h):
-        empty_div = empty_div + "<div class ='row' style='padding:0; margin:0'>"
-        for col in range(w):
-            if cw_list[row][col] == '#':
-                empty_div = empty_div + "<div class ='col-3' style='display: flex; width:30px; height:30px; border:thin solid; background:black; padding: 0; margin: 0;'> </div>"
-            else:
-                empty_div = empty_div + "<div class ='col-3' style='width:30px; height:30px; border:thin solid; background:white; padding: 0; margin: 0;'> </div>"
-        empty_div = empty_div + '</div>'
-
-    """ Create Filled Crossword"""
-    filled_div = ""
-    for row in range(h):
-        filled_div = filled_div + "<div class ='row' style='padding:0; margin:0'>"
-        for col in range(w):
-            if cw_list[row][col] == '#':
-                filled_div = filled_div + "<div class ='col-3' style='width:30px; height:30px; border:thin solid; background:black; padding: 0; margin: 0;'> </div>"
-            else:
-                filled_div = filled_div + "<div class ='col-3' style='width:30px; height:30px; border:thin solid; background:white; padding: 0; margin: 0;'>"\
-                             + cw_list[row][col] + "</div>"
-        filled_div = filled_div + "</div>"
+    html_crossword = div_crossword(cw_list, (h,w))
 
 
     """ Display Crossword """
     context = {
-        "cw": obj,
-        "crossword_empty": empty_div,
-        "crossword_solution": filled_div,
+        "crossword_empty": html_crossword.empty_html,
+        "crossword_solution": html_crossword.filled_html,
+
         "cw_list": cw_list,
         "size": obj.size(),
         "fetched_word_list": word_list,
-        "fetched_words": fetched_words,
-
+        "fetched_words": fetched_words
     }
-
 
     return render(request, 'index.html', context)
