1ff4f40ff6c3f818a09a9baf20b958692563a85f
nikob <nikolaj.bauer@uzh.ch>
2022-05-10 11:04:41 +0200

    Change the correct path to the settings.py

--- crossword_generator/crossword_generator/wsgi.py
+++ crossword_generator/crossword_generator/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crossword_generator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crossword_generator.crossword_generator.settings')
 
 application = get_wsgi_application()
