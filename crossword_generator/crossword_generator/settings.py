7c51b0f8b493be468a072249aaaaa0fef52ec68b
nikob <nikolaj.bauer@uzh.ch>
2022-05-10 11:40:53 +0200

    correct path to catalog, what do we need catalog thing for anyway??

--- crossword_generator/crossword_generator/settings.py
+++ crossword_generator/crossword_generator/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # NEW
-    'catalog.apps.CatalogConfig',
+    'crossword_generator.catalog.apps.CatalogConfig',
     'website',
 ]
 
